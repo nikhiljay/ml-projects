03f8cd26c5bf4a310b646e5e8969f0359f8103cc
Nikhil D'Souza <nikhiljay7@gmail.com>
2018-03-23 23:14:38 -0700

    Added CIFAR10 keras project.

--- keras/CIFAR10/cifar.py
+++ keras/CIFAR10/cifar.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from matplotlib import pyplot as plt
-from keras.datasets import mnist
+from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -14,25 +14,27 @@ import cv2
 import numpy as np
 import operator
 
-# x is image and y is answer
-(x_train, y_train), (x_val, y_val) = mnist.load_data()
+outcomes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-# plt.imshow(x_train[0])
+# x is image and y is outcome
+(x_train, y_train), (x_val, y_val) = cifar10.load_data()
+
+# plt.imshow(x_train[1])
 # plt.show()
 
-# Numbers of rows and coloumns = 28
+# Numbers of rows and coloumns = 32
 img_rows, img_cols = x_train.shape[1], x_train.shape[2]
-num_classes = 10  # one class for each number
+num_classes = 10 # one class for each number
 
-# Reshape images and change to black and white (channel 1)
+# Reshape images and change to rgb (channel 3)
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_val = x_val.reshape(x_val.shape[0], 1, img_rows, img_cols)
+    x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
+    x_val = x_val.reshape(x_val.shape[0], 3, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_val = x_val.reshape(x_val.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
+    x_val = x_val.reshape(x_val.shape[0], img_rows, img_cols, 3)
+    input_shape = (img_rows, img_cols, 3)
 
 # Keras needs type float32 instead of type uint8
 x_train = x_train.astype('float32')
@@ -48,24 +50,19 @@ y_val = np_utils.to_categorical(y_val, num_classes)
 
 # Model architecture
 model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:], activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-# First layer
-model.add(Conv2D(32, (5, 5), input_shape=input_shape, activation='relu')) # Parameters = # of filter/features, layer size, activation function
-model.add(MaxPooling2D(pool_size=(2, 2))) # Pooling filter halves width and height reducing # of parameters (helps control overfitting)
-model.add(Dropout(0.5)) # Randomly sets a fraction of inputs to 0 (works with pooling to help control overfitting)
-
-# Second layer
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
+model.add(Dropout(0.25))
 
-# Third layer
-model.add(Conv2D(128, (1, 1), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Flatten()) # Make weights from layers 1 dimensional
-model.add(Dense(128, activation='relu'))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 # Train model
@@ -77,12 +74,12 @@ epochs = 1
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_val, y_val), verbose=1)
 
 # Test out the model!
-image = cv2.imread("test2.png", 0)
-image = cv2.resize(image, (28, 28))
+image = cv2.imread("test.png")
+image = cv2.resize(image, (32, 32))
 image = image.astype("float") / 255.0
 image = img_to_array(image)
 image = np.expand_dims(image, axis=0)
 
 pred = model.predict(image)[0]
 index, value = max(enumerate(pred), key=operator.itemgetter(1))
-print(str(index) + ', ' + str(value*100) + '% confidence')
+print(outcomes[index] + ', ' + str(value*100) + '% confidence')
